640c1cc871434c949cef9ccd0bcd0a7c5aa87450
zihui <zihui.zhong@polymtl.ca>
2016-11-28 18:42:00 -0500

    Special case

--- Lab4/OriginPointDetection.m
+++ Lab4/OriginPointDetection.m
@@ -11,14 +11,17 @@ if(v~=0)
     b2 = pos2(2)- pos2(1)*a2;
 
     unitaire1 = [-unitaire2(2) unitaire2(1)];
-    a1 = unitaire1(2) / unitaire1(1);
-    b1 = pos1(2)- pos1(1)*a1;
+    if unitaire1(1) ~= 0
+        a1 = unitaire1(2) / unitaire1(1);
+        b1 = pos1(2)- pos1(1)*a1;
 
-    x = (b2-b1)/(a1-a2);
-    y = a1*x+b1;
+        x = (b2-b1)/(a1-a2);
+        y = a1*x+b1;
 
-    aPoint = [x y]';
-
+        aPoint = [x y]';
+    else
+        aPoint= [pos1(1) pos2(2)]';
+    end
 
     a = norm(pos1-aPoint);
     b = norm(pos2-aPoint);
@@ -26,6 +29,7 @@ if(v~=0)
     aCube = v^2/c^2 -1;
     bCube = 2*b*v/c^2;
     cCube = a^2/c^2+b^2/c^2;
+    
     x = roots([aCube bCube cCube]);
     for i = 1:size(x)
         if(x(i)>0)
